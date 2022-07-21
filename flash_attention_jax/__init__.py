192e8d8598a678ca6cb69c84ae6b58ea96288265
Phil Wang <lucidrains@gmail.com>
2022-07-21 09:43:07 -0700

    get cosine sim attention in there for rabe attention

--- flash_attention_jax/__init__.py
+++ flash_attention_jax/__init__.py
@@ -1,1 +1,3 @@
 from flash_attention_jax.flash_attention_jax import flash_attention
+
+from flash_attention_jax.rabe_attention import rabe_attention
--- flash_attention_jax/rabe_attention.py
+++ flash_attention_jax/rabe_attention.py
@@ -51,3 +51,45 @@ def rabe_attention(q, k, v, q_chunk_size = 1024, k_chunk_size = 4096):
 
     _, res = jax.lax.scan(chunk_scanner, init = 0, xs = None, length = math.ceil(q_len / q_chunk_size))
     return res.reshape(q_len, v_dim)
+
+
+# cosine sim attention
+
+def l2norm(t, eps = 1e-6):
+    norm = jnp.linalg.norm(t)
+    return t / (norm + eps)
+
+def _query_chunk_cosine_sim_attention(q, k, v, k_chunk_size = 4096, scale = 16):
+    q_len, k_len, dim, v_dim = q.shape[-2], *k.shape, v.shape[-1]
+
+    k_chunk_size = min(k_chunk_size, k_len)
+
+    @functools.partial(jax.checkpoint, prevent_cse = False)
+    def summarize_chunk(q, k, v):
+        attn_weights = einsum('qd, kd -> qk', q, k) * scale
+        exp_weights = jnp.exp(attn_weights)
+        exp_values = einsum('vf, qv -> qf', v, attn_weights)
+        return (exp_values, exp_weights.sum(axis = -1))
+
+    def chunk_scanner(chunk_idx):
+        k_chunk = lax.dynamic_slice(k, (chunk_idx, 0), slice_sizes=(k_chunk_size, dim))
+        v_chunk = lax.dynamic_slice(v, (chunk_idx, 0), slice_sizes=(k_chunk_size, v_dim))
+        return summarize_chunk(q, k_chunk, v_chunk)
+
+    chunk_values, chunk_weights = jax.lax.map(chunk_scanner, xs = jnp.arange(0, k_len, k_chunk_size))
+
+    all_values = chunk_values.sum(axis = 0)
+    all_weights = jnp.expand_dims(chunk_weights, -1).sum(axis = 0)
+    return all_values / all_weights
+
+def rabe_cosine_sim_attention(q, k, v, q_chunk_size = 1024, k_chunk_size = 4096, scale = 16, eps = 1e-5):
+    q_len, dim, v_dim = *q.shape, v.shape[-1]
+
+    q, k = map(l2norm, (q, k))
+
+    def chunk_scanner(chunk_idx, _):
+        q_chunk = lax.dynamic_slice(q, (chunk_idx, 0), slice_sizes = (min(q_chunk_size, q_len), dim))
+        return (chunk_idx + q_chunk_size, _query_chunk_cosine_sim_attention(q_chunk, k, v, k_chunk_size = k_chunk_size, scale = scale))
+
+    _, res = jax.lax.scan(chunk_scanner, init = 0, xs = None, length = math.ceil(q_len / q_chunk_size))
+    return res.reshape(q_len, v_dim)

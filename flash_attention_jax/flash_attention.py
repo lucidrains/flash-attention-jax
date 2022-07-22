ec93308c756b9b89d18eefb5c3b9e84937b780c2
Phil Wang <lucidrains@gmail.com>
2022-07-22 10:45:07 -0700

    complete flash attention backwards

--- flash_attention_jax/flash_attention.py
+++ flash_attention_jax/flash_attention.py
@@ -8,7 +8,7 @@ from jax import numpy as jnp, lax, jit, grad
 # constants
 
 Q_CHUNK_SIZE = 1024
-K_CHUNK_SIZE = 2048
+K_CHUNK_SIZE = 1024
 
 # flash attention
 
@@ -18,16 +18,18 @@ def _query_chunk_flash_attention(q, k, v):
 
     def chunk_scanner(carries, _):
         chunk_idx, out, row_sum, row_max = carries
-        k_chunk = lax.dynamic_slice(k, (chunk_idx, 0), slice_sizes=(K_CHUNK_SIZE, dim))
-        v_chunk = lax.dynamic_slice(v, (chunk_idx, 0), slice_sizes=(K_CHUNK_SIZE, v_dim))
+        k_chunk_sizes = min(K_CHUNK_SIZE, k_len)
 
-        attn_weights = (q @ k.transpose()) * scale
+        k_chunk = lax.dynamic_slice(k, (chunk_idx, 0), slice_sizes=(k_chunk_sizes, dim))
+        v_chunk = lax.dynamic_slice(v, (chunk_idx, 0), slice_sizes=(k_chunk_sizes, v_dim))
+
+        attn_weights = (q @ k_chunk.transpose()) * scale
 
         block_row_max = jnp.max(attn_weights, axis = -1, keepdims = True)
         exp_weights = jnp.exp(attn_weights - block_row_max)
         block_row_sum = jnp.sum(exp_weights, axis = -1, keepdims = True)
 
-        exp_values = exp_weights @ v
+        exp_values = exp_weights @ v_chunk
 
         new_row_max = jnp.maximum(block_row_max, row_max)
 
@@ -39,7 +41,7 @@ def _query_chunk_flash_attention(q, k, v):
         out = (row_sum / new_row_sum) * exp_row_max_diff * out + \
               (exp_block_row_max_diff / new_row_sum) * exp_values
 
-        return (chunk_idx + K_CHUNK_SIZE, out, new_row_sum, new_row_max), None
+        return (chunk_idx + k_chunk_sizes, out, new_row_sum, new_row_max), None
 
     out = jnp.zeros((q_len, dim))
     row_sum = jnp.zeros((q_len, 1))
@@ -70,22 +72,41 @@ def flash_attention_forward(q, k, v):
     return out, (q, k, v, out, row_sum, row_max)
 
 def _query_chunk_flash_attention_backward(q, k, v, o, do, l, m):
-    dim = q.shape[-1]
+    q_len, dim, k_len, v_dim = *q.shape, *v.shape
+
     scale = 1 / jnp.sqrt(dim)
 
-    sim = q @ k.transpose()
-    sim = sim * scale
-    p = nn.softmax(sim, axis = -1)
+    def chunk_scanner(carries, _):
+        chunk_idx, dq = carries
+        k_chunk_sizes = min(K_CHUNK_SIZE, k_len)
 
-    dv = p.transpose() @ do
-    dp = do @ v.transpose()
+        k_chunk = lax.dynamic_slice(k, (chunk_idx, 0), slice_sizes=(k_chunk_sizes, dim))
+        v_chunk = lax.dynamic_slice(v, (chunk_idx, 0), slice_sizes=(k_chunk_sizes, v_dim))
 
-    D = jnp.sum(do * o, axis = -1, keepdims = True)
-    ds = p * (dp - D)
-    ds = ds * scale
+        attn_weights = q @ k_chunk.transpose()
+        attn_weights = (attn_weights * scale)
+        exp_attn_weights = jnp.exp(attn_weights - m)
+        p = exp_attn_weights / l
 
-    dq = ds @ k
-    dk = ds.transpose() @ q
+        dv_chunk = p.transpose() @ do
+        dp = do @ v_chunk.transpose()
+
+        D = jnp.sum(do * o, axis = -1, keepdims = True)
+        ds = p * (dp - D)
+        ds = ds * scale
+
+        dq_chunk = ds @ k_chunk
+        dk_chunk = ds.transpose() @ q
+
+        return (chunk_idx + k_chunk_sizes, dq + dq_chunk), (dk_chunk, dv_chunk)
+
+    dq = jnp.zeros_like(q)
+
+    (_, dq), (dk, dv) = lax.scan(chunk_scanner, init = (0, dq), xs = None, length = math.ceil(k_len / K_CHUNK_SIZE))
+
+    dq = dq.reshape(q_len, dim)
+    dk = dk.reshape(k_len, v_dim)
+    dv = dv.reshape(k_len, v_dim)
 
     return dq, dk, dv
 
@@ -98,14 +119,19 @@ def flash_attention_backward(res, do):
     dk = jnp.zeros_like(k)
     dv = jnp.zeros_like(v)
 
+    m = m.reshape(q_len, 1)
+    l = l.reshape(q_len, 1)
+
     def chunk_scanner(carries, _):
         chunk_idx, dk, dv = carries
 
         q_chunk = lax.dynamic_slice(q, (chunk_idx, 0), slice_sizes = (min(Q_CHUNK_SIZE, q_len), q.shape[-1]))
+        m_chunk = lax.dynamic_slice(m, (chunk_idx, 0), slice_sizes = (min(Q_CHUNK_SIZE, q_len), 1))
+        l_chunk = lax.dynamic_slice(l, (chunk_idx, 0), slice_sizes = (min(Q_CHUNK_SIZE, q_len), 1))
         o_chunk = lax.dynamic_slice(o, (chunk_idx, 0), slice_sizes = (min(Q_CHUNK_SIZE, q_len), o.shape[-1]))
         do_chunk = lax.dynamic_slice(do, (chunk_idx, 0), slice_sizes = (min(Q_CHUNK_SIZE, q_len), do.shape[-1]))
 
-        dq_chunk, dk_chunk, dv_chunk = _query_chunk_flash_attention_backward(q_chunk, k, v, o_chunk, do_chunk, l, m)
+        dq_chunk, dk_chunk, dv_chunk = _query_chunk_flash_attention_backward(q_chunk, k, v, o_chunk, do_chunk, l_chunk, m_chunk)
         return (chunk_idx + Q_CHUNK_SIZE, dk + dk_chunk, dv + dv_chunk), dq_chunk
 
     (_, dk, dv), dq = lax.scan(chunk_scanner, init = (0, dk, dv), xs = None, length = math.ceil(q_len / Q_CHUNK_SIZE))
